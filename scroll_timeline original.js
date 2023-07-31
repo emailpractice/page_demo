77c80c804f331dd8c0628f1aa483c60ad0450bab
seashell14711 <510478@gm.tnfsh.tn.edu.tw>
2023-07-31 13:43:52 +0800

    1

--- scroll_timeline original.js
+++ scroll_timeline original.js
@@ -32,21 +32,7 @@ function show_scroll_down() {
   elem_downwardline.className = 'pls_scroll_down'
 
 }
-/*
-function not_show_scroll_text() {
 
-  let elem_scroll_text = document.querySelector('#ele_scroll_text')
-  elem_scroll_text.className = 'not_show_scroll_text_vertical'
-
-
-}
-function show_scroll_down_text() {
-
-  let element_scroll_text = document.querySelector('#ele_scroll_text')
-  element_scroll_text.className = 'show_scroll_text_vertical'
-
-}
- */
 function not_show_logo1() {
 
   let elem_logo1 = document.querySelector('#ele_logo1')
@@ -81,7 +67,6 @@ window.addEventListener('scroll', () => {
   if (window.scrollY != 0 && a == true) {
     add_black_overlay()  /*變模糊的函數*/
     not_show_scroll_down()
-    /*not_show_scroll_text()*/
     not_show_logo1()
     not_show_start_your_order_text()
     a = false
@@ -90,7 +75,6 @@ window.addEventListener('scroll', () => {
 
     remove_black_overlay()
     show_scroll_down()
-    /*show_scroll_down_text()*/
     show_logo1()
     show_start_your_order_text()
     a = true
