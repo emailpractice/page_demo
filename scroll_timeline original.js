398fa33e5c68dfd46d594b39ddb331b669e04579
seashell14711 <510478@gm.tnfsh.tn.edu.tw>
2023-08-30 16:54:18 +0800

    表單測試

--- scroll_timeline original.js
+++ scroll_timeline original.js
@@ -73,6 +73,18 @@ window.addEventListener('scroll', () => {
 })
 
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwpQ_mRRbIlBo8O9oOp7pAgi4tSUKw8_LuajqZSCqVnMyBGYWTRxPDIgBCL2qqmdS2f/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! your form is submitted successfully."))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
+})
+
 
 
 
