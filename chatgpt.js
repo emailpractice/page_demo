d0afd7a8a13544b384c272a27534dc267fb9df6b
seashell14711 <510478@gm.tnfsh.tn.edu.tw>
2023-09-01 12:56:36 +0800

    1

--- chatgpt.js
+++ chatgpt.js
@@ -6,45 +6,58 @@ const elem_start_your_order_text = document.querySelector('#ele_start_your_order
 const elem_anchorbutton = document.querySelector('#ele_anchorbutton');
 const elem_submit = document.querySelector('#submit');
 
-function updateElements(blur, showScrollDown, showLogo1, showStartText, position) {
-    elem_bg.className = blur ? 'bground_blur' : 'bground_not_blur';
-    elem_overlay.className = blur ? 'black_overlay' : 'black_not_overlay';
-    elem_downwardline.className = showScrollDown ? 'pls_scroll_down' : 'pls_scroll_down_not_showed';
-    elem_logo1.className = showLogo1 ? 'show_logo1' : 'not_show_logo1';
-    elem_start_your_order_text.className = showStartText ? 'show_start_your_order_text' : 'not_show_start_your_order_text';
-    elem_anchorbutton.className = position === 'topright' ? 'anchor_button_order_page_topright' : 'anchor_button_order_page_center';
+
+function add_black_overlay() {
+    elem_bg.classList.add('bground_blur');
+    elem_overlay.classList.add('black_overlay');
+    elem_downwardline.classList.add('pls_scroll_down_not_showed');
+    elem_logo1.classList.add('not_show_logo1');
+    elem_start_your_order_text.classList.add('not_show_start_your_order_text');
+    elem_anchorbutton.classList.add('anchor_button_order_page_topright');
 }
 
-var a = true;
+function remove_black_overlay() {
+    elem_bg.classList.remove('bground_blur');
+    elem_overlay.classList.remove('black_overlay');
+    elem_downwardline.classList.remove('pls_scroll_down_not_showed');
+    elem_logo1.classList.remove('not_show_logo1');
+    elem_start_your_order_text.classList.remove('not_show_start_your_order_text');
+    elem_anchorbutton.classList.remove('anchor_button_order_page_topright');
+}
+
+
+
+var a = true
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY !== 0 && a) {
-        updateElements(true, false, false, false, 'topright');
-        a = false;
-    } else if (window.scrollY === 0) {
-        updateElements(false, true, true, true, 'center');
-        a = true;
-    }
-});
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwpQ_mRRbIlBo8O9oOp7pAgi4tSUKw8_LuajqZSCqVnMyBGYWTRxPDIgBCL2qqmdS2f/exec';
-const form = document.forms['contact-form'];
+    if (window.scrollY != 0 && a == true) {
+        add_black_overlay()  /*變模糊的函數*/
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    elem_submit.id = 'loader';
-    elem_submit.value = '';
-    try {
-        const response = await fetch(scriptURL, { method: 'POST', body: new FormData(form) });
-        if (response.ok) {
-            alert('收到您的訂餐訊息了，餐點送到時會以電話通知');
-            window.location.reload();
-        } else {
-            throw new Error('Request failed');
-        }
-    } catch (error) {
-        console.error('Error!', error.message);
-    } finally {
-        elem_submit.id = '';
+        a = false
     }
-});
+    else if (window.scrollY == 0) { /*變清晰的函數*/
+
+        remove_black_overlay()
+        a = true
+    }
+})
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwpQ_mRRbIlBo8O9oOp7pAgi4tSUKw8_LuajqZSCqVnMyBGYWTRxPDIgBCL2qqmdS2f/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    elem_submit.id = 'loader' /*送出按鈕變成轉圈圈*/
+    elem_submit.value = '';  /*'送出'兩字變成空白*/
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("收到您的訂餐訊息了，餐點送到時會以電話通知"))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
